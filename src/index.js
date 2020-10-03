6fa3f9456fc1ddefeccdbe9375ee45586646e1f0
SergRudovich <slunrew@tut.by>
2020-10-03 12:10:44 +0300

    feat: add min max and avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+    let min = array[0];
+    if (array.length == 0) return 0;
+    for (let current of array) {
+        if (current < min) min = current;
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+    let max = array[0];
+    if (array.length == 0) return 0;
+    for (let current of array) {
+        if (current > max) max = current;
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+    let avg = 0;
+    if (array.length == 0) return 0;
+    for (let current of array) {
+        avg += current;
+    }
+    avg /= array.length;
+    return avg;
 }
